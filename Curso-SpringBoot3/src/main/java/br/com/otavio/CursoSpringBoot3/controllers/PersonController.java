bff5657a00170e91468bc97d84844b9903c8d9b0
Otavio Marques <marquesotavio251204@gmail.com>
2023-07-18 19:37:12 -0300

    completing crud

--- Curso-SpringBoot3/src/main/java/br/com/otavio/CursoSpringBoot3/controllers/PersonController.java
+++ Curso-SpringBoot3/src/main/java/br/com/otavio/CursoSpringBoot3/controllers/PersonController.java
@@ -3,16 +3,21 @@ package br.com.otavio.CursoSpringBoot3.controllers;
 import br.com.otavio.CursoSpringBoot3.model.Person;
 import br.com.otavio.CursoSpringBoot3.services.PersonService;
 
+import java.util.List;
+
+import javax.print.attribute.standard.Media;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
+@RequestMapping("/person")
 public class PersonController {
 	
 	@Autowired
@@ -24,5 +29,37 @@ public class PersonController {
 	public Person findById(@PathVariable(value = "id") String id) {
 		return service.findById(id);
 	}
-
+	
+	@RequestMapping(
+			method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return service.findAll();
+	}
+	
+	@RequestMapping(
+		method = RequestMethod.POST,
+		consumes = MediaType.APPLICATION_JSON_VALUE,//Consome
+		produces = MediaType.APPLICATION_JSON_VALUE//Produz
+			)
+	public Person creating(@RequestBody Person person) {
+		return service.create(person);
+	}
+	
+	@RequestMapping(
+			method = RequestMethod.PUT,
+			consumes = MediaType.APPLICATION_JSON_VALUE,//Consome
+			produces = MediaType.APPLICATION_JSON_VALUE//Produz
+			)
+	public Person update(@RequestBody Person person) {
+		return service.update(person);
+	}
+	
+	@RequestMapping(
+			value = "/{id}",
+			method = RequestMethod.DELETE
+			)
+	public void delete(@RequestParam(value = "id") String id) {
+		
+	}
 }
--- Curso-SpringBoot3/src/main/java/br/com/otavio/CursoSpringBoot3/services/PersonService.java
+++ Curso-SpringBoot3/src/main/java/br/com/otavio/CursoSpringBoot3/services/PersonService.java
@@ -1,5 +1,7 @@
 package br.com.otavio.CursoSpringBoot3.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -28,5 +30,54 @@ public class PersonService {
     	
     	return person;
     }
+    
+    public List<Person> findAll() {
+    	
+    	logger.info("Fiding all persin!");
+    	
+    	List<Person> list = new ArrayList<>();
+    	
+    	for(int i = 0; i<8; i++) {
+
+        	Person person = mockPerson(i);
+        	
+        	list.add(person);
+        
+    	}
+    	
+    	return list;
+    }
+    
+    public Person create(Person person) {
+    	person.setId(counter.incrementAndGet());
+    	logger.info("Creating one Operson");
+    	
+    	return person;
+    }
+    
+    public Person update(Person person) {
+    	logger.info("uptadeting one Operson");
+    	person.setId(counter.incrementAndGet());
+    	
+    	return person;
+    }
+    
+    public void delete(String id) {
+    	logger.info("Deleting one person");
+    	
+    	
+    }
+
+	private Person mockPerson(int i) {
+		Person person = new Person(
+    			counter.incrementAndGet(), 
+    			"Otavio" +i,
+    			"Marques" +i,
+    			"Moreira Sales - Pr - Br",
+    			"M"
+    			);
+		
+		return person;
+	}
 
 }
