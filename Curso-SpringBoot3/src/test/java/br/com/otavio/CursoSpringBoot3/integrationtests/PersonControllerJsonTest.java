a8d37ced0b27024da1e6d8aef3da2f66c836549b
Otavio Marques <marquesotavio251204@gmail.com>
2023-08-17 21:49:07 -0300

    test PersonControllerJson

--- Curso-SpringBoot3/src/test/java/br/com/otavio/CursoSpringBoot3/integrationtests/PersonControllerJsonTest.java
+++ Curso-SpringBoot3/src/test/java/br/com/otavio/CursoSpringBoot3/integrationtests/PersonControllerJsonTest.java
@@ -3,28 +3,74 @@ package br.com.otavio.CursoSpringBoot3.integrationtests;
 
 import br.com.otavio.CursoSpringBoot3.configs.TestsConfigs;
 import br.com.otavio.CursoSpringBoot3.integrationtests.testcontainers.AbstractIntegrationTest;
+import br.com.otavio.CursoSpringBoot3.integrationtests.vo.PersonVO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.TestMethodOrder;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(OrderAnnotation.class)
 public class PersonControllerJsonTest extends AbstractIntegrationTest{
-	
+
+	private static RequestSpecification specification;
+	private static ObjectMapper objectMapper;
+	private static PersonVO person;
+
+	@BeforeAll
+	public static void setup(){
+		objectMapper = new ObjectMapper();
+		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+		person = new PersonVO();
+	}
+
 	@Test
-	public void shouldDisplaySwaggerUiPage() {
-		var content = RestAssured.given()
-			.basePath("/swgger-ui/index.html")
-			.port(TestsConfigs.SERVER_PORT)
-			.when()//quando
-				.get()//for metodo get
-			.then()//espero
-				.statusCode(200)//status 200
-			.extract()
-				.body()
-					.asString();
-		
-		Assertions.assertTrue(content.contains("Swagger UI"));
+	@Order(1)
+	public void testCreate() throws JsonMappingException, JsonProcessingException {
+		mockperson();
+
+		specification = new RequestSpecBuilder()
+				.addHeader(TestsConfigs.HEADER_PARAM_ORIGIN, "https://otavio.com.br")
+				.setBasePath("/api/person/v1")
+				.setPort(TestsConfigs.SERVER_PORT)
+					.addFilter(new RequestLoggingFilter(LogDetail.ALL))
+					.addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+				.build();
+		System.out.println(person.getFirstName());
+		var content = RestAssured.given().spec(specification)
+				.contentType(TestsConfigs.CONTENT_TYPE_JSON)
+					.body(person)
+					.when()
+					.post()
+				.then()
+					.statusCode(200)
+						.extract()
+						.body()
+							.asString();
+
+		PersonVO createdPerson = objectMapper.readValue(content, PersonVO.class);
+		person = createdPerson;
+
+		Assertions.assertEquals("Richard", createdPerson.getFirstName());
+
+	}
+
+	private void mockperson() {
+		person.setFirstName("Richard");
+		person.setLastName("Stallman");
+		person.setAddress("New York City");
+		person.setGender("m");
 	}
 
 }
--- Curso-SpringBoot3/src/test/java/br/com/otavio/CursoSpringBoot3/integrationtests/vo/PersonVO.java
+++ Curso-SpringBoot3/src/test/java/br/com/otavio/CursoSpringBoot3/integrationtests/vo/PersonVO.java
@@ -6,42 +6,29 @@ import com.github.dozermapper.core.Mapping;
 import org.springframework.hateoas.RepresentationModel;
 
 import java.io.Serializable;
-import java.util.Objects;
+
 
 public class PersonVO implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
 
     private Long id;
     private String firstName;
-    private String lastname;
+    private String lastName;
     private String address;
     private String gender;
 
-    public PersonVO() {
-    	
+    public PersonVO() {}
+
+    public Long getId() {
+        return id;
     }
 
-    public PersonVO(Long id, String firstName, String lastname, String address, String gender) {
+    public void setId(Long id) {
         this.id = id;
-        this.firstName = firstName;
-        this.lastname = lastname;
-        this.address = address;
-        this.gender = gender;
     }
 
-    
-
-    public Long getid() {
-		return id;
-	}
-
-	public void setid(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
+    public String getFirstName() {
         return firstName;
     }
 
@@ -49,12 +36,12 @@ public class PersonVO implements Serializable {
         this.firstName = firstName;
     }
 
-    public String getLastname() {
-        return lastname;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getAddress() {
@@ -74,22 +61,51 @@ public class PersonVO implements Serializable {
     }
 
     @Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (!super.equals(obj))
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PersonVO other = (PersonVO) obj;
-		return Objects.equals(id, other.id);
-	}
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((address == null) ? 0 : address.hashCode());
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        return result;
+    }
 
     @Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + Objects.hash(id);
-		return result;
-	}
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        PersonVO other = (PersonVO) obj;
+        if (address == null) {
+            if (other.address != null)
+                return false;
+        } else if (!address.equals(other.address))
+            return false;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (gender == null) {
+            if (other.gender != null)
+                return false;
+        } else if (!gender.equals(other.gender))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        return true;
+    }
 }
